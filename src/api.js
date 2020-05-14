3b2d49eab9470306dd058b829240311c1cb128a6
Bruno Souto <bruno.csouto@gmail.com>
2020-05-13 22:56:25 -0300

    Add search function

--- src/api.js
+++ src/api.js
@@ -5,24 +5,3 @@ const api = axios.create({
 });
 
 export default api;
-
-class Api {
-    static async getAllCountriesInfo() {
-        try {
-            const response = await axios.get(`https://restcountries.eu/rest/v2/region/americas`);
-
-            console.log(response)
-        } catch(err) {
-            console.warn("Error from API request", err);
-        }
-    }
-    static async getCountryInfo(countryCode) {
-        try {
-            const response = await axios.get(`https://restcountries.eu/rest/v2/alpha/${countryCode}`);
-    
-            console.log(response)
-        } catch(err) {
-            console.warn("Error from API request:", err);
-        }
-    }
-}
